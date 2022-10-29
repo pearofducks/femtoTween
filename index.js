8f17dce6fc64204c61101d6832320046a13adeb7
Yofou <exodias1965@gmail.com>
2022-10-29 10:08:47 +0100

    feat: Initial start on adding tweening delay

--- index.js
+++ index.js
@@ -15,26 +15,33 @@ import { easeInOutQuart } from './ease.js'
  * @arg {number} [opts.time = 400] - how long the tween should last
  * @arg {function} [opts.done] - function called when tweening is finished
  * @arg {function} [opts.ease] - function used for easing the tween
+ * @arg {Number} [opts.delay] - Delays the tweened
  * @returns {function} - callback that can stop a tween in progress
  */
-export function tween(from, to, cb, { time = 400, done, ease = easeInOutQuart } = {}) {
+export function tween(from, to, cb, { time = 400, done, ease = easeInOutQuart, delay } = {}) {
   const windowExists = (typeof window !== 'undefined')
   let stopped = false
   const stop = () => stopped = true
   const diff = from - to
   let start = null
+  /**
+   * Step function
+   * @arg {Number} timestamp - the time elsaped since the animation started
+   */
   function step(timestamp) {
-    if (stopped) return
-    if (!start) start = timestamp
-    const progress = timestamp - start
-    const percentage = Math.min(progress / time, 1)
-    cb(from - ease(percentage) * diff)
-    if (progress < time) {
-      window.requestAnimationFrame(step)
-    } else {
-      cb(to)
-      done && done()
-    }
+    setTimeout(() => {
+      if (stopped) return
+      if (!start) start = timestamp
+      const progress = timestamp - start
+      const percentage = Math.min(progress / time, 1)
+      cb(from - ease(percentage) * diff)
+      if (progress < time) {
+        window.requestAnimationFrame(step)
+      } else {
+        cb(to)
+        done && done()
+      }
+    }, delay ?? 0)
   }
   windowExists && window.requestAnimationFrame(step)
   return stop

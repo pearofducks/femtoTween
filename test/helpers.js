eae630694c3d97b2d41d9f0dd514f5920d66951b
Yofou <exodias1965@gmail.com>
2022-10-29 10:43:54 +0100

    feat: Added Test case to check for delay

--- test/helpers.js
+++ test/helpers.js
@@ -1,1 +1,5 @@
 export const waitRAF = () => new Promise(resolve => requestAnimationFrame(() => requestAnimationFrame(resolve)))
+/**
+ * @arg {Number} delay - number of seconds to wait for
+ */
+export const waitTimeout = (delay) => new Promise(resolve => setTimeout(resolve, delay))
